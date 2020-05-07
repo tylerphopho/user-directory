a1147653ffae01bb4584242b94e3485932b26a47
tylerphopho <tyler.phopho@gmail.com>
2020-05-06 19:11:53 -0600

    removed app.listen

--- server.js
+++ server.js
@@ -14,21 +14,17 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-}
+
 
 app.get("*", function(req, res) {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 
-
 app.use("/", apiRouting);
 
-app.listen(PORT, () => {
-    console.log(`Currently listening to http:localhost:${PORT}`)
-});
-
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
 mongoose.connect(process.env.MONGODB_URI || "mongodb://tylerphopho:password1@ds125578.mlab.com:25578/heroku_pdnwd75r",{
     useUnifiedTopology: true,
     useNewUrlParser: true,
